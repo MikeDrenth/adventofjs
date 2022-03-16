9408e6dbeb7872a95e44888036ddf3d2de8490a0
Mike Drenth <mike@digizijn.nl>
2022-03-16 15:05:45 +0100

    Init project

---
+++ STARTER-FILES/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const btnStart = document.querySelector(".start");
+const btnnSetting = document.querySelector(".settings");
+const inputMinuts = document.querySelector(".minutes input");
+const inputSeconds = document.querySelector(".seconds input");
